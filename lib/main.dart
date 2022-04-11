4c822aa8ab105251d8f00012e7e2c384118cd0d6
LalremLian <amongbt@gmail.com>
2022-04-11 12:30:18 +0600

    Worked on main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'dart:async';
 import 'dart:convert';
-
+import 'package:lottie/lottie.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
-import 'package:mini_dictionary/view/defination_page.dart';
+import 'package:double_back_to_close_app/double_back_to_close_app.dart';
 import 'package:mini_dictionary/view/splash_screen_page.dart';
 
 void main() => runApp(const MyApp());
@@ -14,12 +14,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const SplashScreen(),
+      home: SplashScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
@@ -33,7 +30,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   final String _token = "c99413a763f9861f62fad84a38adf948c9e50b71";
 
   final TextEditingController _controller = TextEditingController();
@@ -44,6 +40,7 @@ class _MyHomePageState extends State<MyHomePage> {
   late Timer _debounce;
   String error = 'No Defination found.';
 
+  //.............................................................Search Function
   _search() async {
     if (_controller.text.isEmpty) {
       _streamController.add(null);
@@ -51,14 +48,15 @@ class _MyHomePageState extends State<MyHomePage> {
     }
 
     _streamController.add("waiting");
-    Response response = await get(Uri.https("owlbot.info", "/api/v4/dictionary/"+ _controller.text.trim()),
+    Response response = await get(
+        Uri.https(
+            "owlbot.info", "/api/v4/dictionary/" + _controller.text.trim()),
         headers: {"Authorization": "Token " + _token});
 
-    if(response.statusCode == 200)
-      {
-        _streamController.add(json.decode(response.body));
-      }
-      error = "sdf";
+    if (response.statusCode == 200) {
+      _streamController.add(json.decode(response.body));
+    }
+    error = "sdf";
   }
 
   @override
@@ -68,13 +66,26 @@ class _MyHomePageState extends State<MyHomePage> {
     _streamController = StreamController();
     _stream = _streamController.stream;
   }
-  String firstUpperCase(String str)
-  {
-    String a = str.substring(0,1);
+
+  String firstUpperCase(String str) {
+    String a = str.substring(0, 1);
+
     a = a.toUpperCase();
+
     String b = str.substring(1);
 
-    return a+b;
+    return a + b;
+  }
+
+  String checkIfNull(String str) {
+    return str;
+  }
+  String exampleHeading(String str) {
+    if(str!='')
+      {
+        str = "Example : ";
+      }
+    return str;
   }
 
   @override
@@ -95,9 +106,9 @@ class _MyHomePageState extends State<MyHomePage> {
                     borderRadius: BorderRadius.circular(24.0),
                   ),
                   child: TextFormField(
-                    //.............................................Search Action
+                    //.................................Search Action In keyboard
                     textInputAction: TextInputAction.search,
-                    onFieldSubmitted: (value){
+                    onFieldSubmitted: (value) {
                       _search();
                     },
                     onChanged: (String text) {
@@ -129,71 +140,106 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ),
       ),
-      body: Container(
-        margin: const EdgeInsets.all(8.0),
-        child: StreamBuilder(
-          stream: _stream,
-          builder: (BuildContext ctx, AsyncSnapshot snapshot) {
-            if (snapshot.data == null) {
-              return const Center(
-                child: Text("Enter a search word"),
-              );
-            }
-
-            if (snapshot.data == "waiting") {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-
-            return ListView.builder(
-              itemCount: snapshot.data["definitions"].length,
-              itemBuilder: (BuildContext context, int index) {
-                return Card(
-                  color: Colors.grey[100],
-                  child: ListBody(
-                    children: <Widget>[
-                      Container(
-                        color: Colors.grey[300],
-                        child: ListTile(
-                          leading: snapshot.data["definitions"][index]["image_url"] == null
-                              ? null
-                              : Image.network(snapshot.data["definitions"][index]["image_url"],height: 120,),
-                          title: Text(_controller.text.trim() + " (" + snapshot.data["definitions"][index]["type"] + ")",style: const TextStyle(fontSize: 18),),
-                        ),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: Text("Definition :",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 8.0,bottom: 8.0),
-                            child: Text(firstUpperCase(snapshot.data["definitions"][index]["definition"]),
-                              style: const TextStyle(fontSize: 16),),
-                          ),
-                          const Padding(
-                            padding: EdgeInsets.only(top: 8.0, left: 8.0),
-                             child: Text("Example :",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
-                          ),
-                        ],
-                      ),
-                      ListTile(
-                        leading: snapshot.data["definitions"][index]["example"] == null
-                            ? null
-                            :Padding(
-                          padding: const EdgeInsets.only(bottom: 4.0),
-                          child: Text(firstUpperCase(snapshot.data["definitions"][index]["example"]),style: const TextStyle(fontSize: 16),),
-                        ),
-                      )
-                    ],
-                  ),
+      //................................................DoubleBack Functionality
+      body: DoubleBackToCloseApp(
+        snackBar: const SnackBar(
+          content: Text('Tap back again to leave'),
+        ),
+        child: Container(
+          margin: const EdgeInsets.all(8.0),
+          child: StreamBuilder(
+            stream: _stream,
+            builder: (BuildContext ctx, AsyncSnapshot snapshot) {
+              if (snapshot.data == null) {
+                return const Center(
+                  child: Text("Enter a search word"),
                 );
-              },
-            );
-          },
+              }
+
+              if (snapshot.data == "waiting") {
+                return Center(
+                  child: Lottie.asset('assets/searching.json'),
+                );
+              }
+
+              return ListView.builder(
+                itemCount: snapshot.data["definitions"].length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                    color: Colors.grey[100],
+                    child: ListBody(
+                      children: <Widget>[
+                        Container(
+                          color: Colors.grey[300],
+                          child: ListTile(
+                            leading: snapshot.data["definitions"][index]
+                                        ["image_url"] ==
+                                    null
+                                ? null
+                                : Image.network(
+                                    snapshot.data["definitions"][index]
+                                        ["image_url"],
+                                    height: 120,
+                                  ),
+                            title: Text(
+                              _controller.text.trim() +
+                                  " (" +
+                                  snapshot.data["definitions"][index]["type"] +
+                                  ")",
+                              style: const TextStyle(fontSize: 16),
+                            ),
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Padding(
+                              padding: EdgeInsets.only(
+                                  top: 8.0, left: 8.0, bottom: 4.0),
+                              child: Text(
+                                "Definition :",
+                                style: TextStyle(
+                                    fontSize: 16, fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(left: 8.0, bottom: 4.0),
+                              child: Text(
+                                firstUpperCase(snapshot.data["definitions"]
+                                    [index]["definition"]),
+                                style: const TextStyle(fontSize: 15),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 8.0, top: 10.0),
+                              child: Text(
+                                exampleHeading(snapshot.data["definitions"][index]
+                                ["example"] ??
+                                    ''),
+                                style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 8.0, bottom: 6.0, top: 4.0),
+                              child: Text(
+                                checkIfNull(snapshot.data["definitions"][index]
+                                        ["example"] ??
+                                    ''),
+                                style: const TextStyle(fontSize: 15),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              );
+            },
+          ),
         ),
       ),
     );
